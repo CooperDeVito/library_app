0402b99477223da1e15a1165cbf76dd43533d515
Cooper DeVito <ced3rq@virginia.edu>
2021-05-27 17:13:16 -0400

    fixing

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 //backend for the databases demo
 const express = require('express');
-
 const cors = require('cors');
+
 const port = 8080;
 const app = express();
+const db = require('./firebase');
 
-app.use(cors());
+app.use(express.json());
+app.use(cors({ origin: true })); //pretty much allow any cross sharing
 
 app.get('/', (req, res) => {
 	res.send('Whats up');
--- server/firebase.js
+++ server/firebase.js
@@ -2,11 +2,11 @@
 const admin = require('firebase-admin');
 const serviceAccount = require('./firebase_key.json');
 
-//activate credentials 
-admin.initializeApp({ 
-    credential:admin.credential.cert(serviceAccount);
+//activate credentials
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
 });
 
-const database = admin.firestore(); 
+const database = admin.firestore();
 
-module.exports = database; 
+module.exports = database;
