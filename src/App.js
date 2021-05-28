afbe7c9e0b8c3fa13e26486d36c0e9529c0b42a8
Cooper DeVito <ced3rq@virginia.edu>
2021-05-28 14:03:02 -0400

    post from client side: chck

--- src/App.js
+++ src/App.js
@@ -1,8 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-	return <div className="App"></div>;
+	const newTitle = 'The Boys in The Boat';
+	const newAuthor = 'Daniel James Brown';
+
+	const newBook = {
+		Title: newTitle,
+		Author: newAuthor,
+	};
+
+	const [allBooks, setAllBooks] = useState();
+	useEffect(() => {
+		fetch('http://localhost:8080/books/get')
+			.then((response) => {
+				return response.json();
+			})
+			.then((object) => {
+				setAllBooks(object);
+			});
+	}, []);
+
+	useEffect(() => {
+		fetch('http://localhost:8080/books/add', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(newBook),
+		})
+			.then((response) => {
+				return response.json();
+			})
+			.then((object) => {
+				console.log('Success', object);
+			});
+	}, []);
+
+	if (!allBooks) {
+		return <div> Loading... </div>;
+	}
+	return (
+		<div className="App">
+			{allBooks.map((book) => (
+				<pre>{JSON.stringify(book)}</pre>
+			))}
+		</div>
+	);
 }
 
 export default App;
