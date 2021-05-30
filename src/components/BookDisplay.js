fc551ed405789d94ec0ab5ad7ed6cc3320b01902
Cooper DeVito <ced3rq@virginia.edu>
2021-05-30 14:21:13 -0400

    Display in datagrid

--- src/components/BookDisplay.js
+++ src/components/BookDisplay.js
@@ -1,24 +1,54 @@
 import React from 'react';
-import { Paper } from '@material-ui/core';
+import { Paper, Card } from '@material-ui/core';
+import { DataGrid } from '@material-ui/data-grid';
 
 function BookDisplay(props) {
+	const columns = [
+		{ field: 'id', headerName: 'ID', width: 70 },
+		{ field: 'title', headerName: 'Title', width: 150 },
+		{ field: 'subtitle', headerName: 'Subtitle', width: 400 },
+		{ field: 'authors', headerName: 'Authors', width: 250 },
+		{ field: 'rating', headerName: 'Rating', width: 130 },
+	];
+
+	let count = 1;
+	let theRows = [];
+	if (props.booksFound) {
+		console.log('books found');
+		const rows = props.booksFound.map((book) => {
+			return {
+				id: count++,
+				title: book.volumeInfo.title,
+				subtitle: book.volumeInfo.subtitle,
+				authors: book.volumeInfo.authors,
+				rating: book.volumeInfo.averageRating,
+			};
+		});
+		theRows = rows;
+	}
 	if (!props.booksFound) {
 		return <h1> Nothing to see here... </h1>;
 	}
+
 	return (
 		<Paper>
-			{props.booksFound.map((book) => {
-				return (
-					<Paper>
-						<h2> {JSON.stringify(book.volumeInfo.title)} </h2>
-						<h3> Author: </h3>
-						{book.volumeInfo.authors.map((author) => {
-							return <h3> {author} </h3>;
-						})}
-					</Paper>
-				);
-			})}
+			<div style={{ height: 800, width: '100%' }}>
+				<DataGrid rows={theRows} columns={columns} pageSize={10} />
+			</div>
 		</Paper>
+		// <Paper>
+		// 	{props.booksFound.map((book) => {
+		// 		return (
+		// 			<Card raised="true">
+		// 				<h2> {JSON.stringify(book.volumeInfo.title)} </h2>
+		// 				<h3> Author: </h3>
+		// 				{book.volumeInfo.authors.map((author) => {
+		// 					return <h3> {author} </h3>;
+		// 				})}
+		// 			</Card>
+		// 		);
+		// 	})}
+		// </Paper>
 	);
 }
 
